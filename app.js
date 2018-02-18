531c2d57e854b22c1ce0f02ff526c3f5c412a3df
Kevin Godell <kevin.godell@gmail.com>
2018-02-18 13:39:08 -0600

    added static paths

--- app.js
+++ app.js
@@ -28,12 +28,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 //app.use('/users', users);
 app.use('/hls', hls);
 app.use('/mp4', mp4);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/flv.js/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/hls.js/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/material-design-lite/dist')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

332ddd9c9d8de2c1950a8e938fe5e372b78ec31c
Kevin Godell <kevin.godell@gmail.com>
2018-02-18 04:14:08 -0600

    launch browser, add routes

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 'use strict';
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';//change to production before packaging to binary
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const opn = require('opn');
+opn('http://localhost:8181');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+//const users = require('./routes/users');
+const hls = require('./routes/hls');
+const mp4 = require('./routes/mp4');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +31,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
+app.use('/hls', hls);
+app.use('/mp4', mp4);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });

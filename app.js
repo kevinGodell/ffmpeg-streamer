15cdd20e6e1c36454dc2aad2815714f7395e722f
Kevin Godell <kevin.godell@gmail.com>
2018-02-18 23:07:44 -0600

    add assets route

--- app.js
+++ app.js
@@ -3,21 +3,64 @@
 process.env.NODE_ENV = 'production';//change to production before packaging to binary
 
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(http/*, {origins: allowedOrigins}*/);
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const opn = require('opn');
-opn('http://localhost:8181');
-
+const debug = require('debug')('ip-cam-tester:server');
+const port = normalizePort(process.env.PORT || '8181');
+const ejs = require('ejs');
 const index = require('./routes/index');
-//const users = require('./routes/users');
 const hls = require('./routes/hls');
 const mp4 = require('./routes/mp4');
 const mjpeg = require('./routes/mjpeg');
+const assets = require('./routes/assets');
 
-const app = express();
+app.set('port', port);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+    console.log('Listening on ' + bind);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,15 +74,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
-//app.use('/users', users);
 app.use('/hls', hls);
 app.use('/mp4', mp4);
 app.use('/mjpeg', mjpeg);
-
+app.use('/assets', assets);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules/flv.js/dist')));
-app.use(express.static(path.join(__dirname, 'node_modules/hls.js/dist')));
-app.use(express.static(path.join(__dirname, 'node_modules/material-design-lite/dist')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,7 +88,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

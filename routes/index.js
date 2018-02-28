e9666b62deb3b708e8e9bc73d1c006a3fa71029e
Kevin Godell <kevin.godell@gmail.com>
2018-02-27 20:07:57 -0600

    passed values from client to server and back, reworked logic to process params

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,15 @@ const M4F = require('mp4frag');
 const P2J = require('pipe2jpeg');
 const packageJson = require('../package');
 const title = `${packageJson.name} ver: ${packageJson.version}`;
+let values = null;
 
-function renderIndex(res, msg) {
+function renderIndex(res, msg, vals) {
     const app = res.app;
     res.render('index', {
         title: title,
         subTitle: `ffmpeg ver: ${app.get('ffmpegVersion')}`,
-        message: msg
+        message: msg,
+        values: vals
     });
 }
 
@@ -24,33 +26,23 @@ function renderVideo(res, params) {
     });
 }
 
-function renderInputError(res, msg) {
-    const app = res.app;
-    res.render('index', {
-        title: title,
-        subTitle: `ffmpeg ver: ${app.get('ffmpegVersion')}`,
-        message: msg
-    });
-}
-
 router.get('/', function (req, res) {
     const app = req.app;
-
     if (!app.get('ffmpegVersion')) {
         res.render('install', {
             title: 'Dependency Error',
             subTitle: 'FFMPEG not found on system.',
-            message: 'Would like to automatically install ffmpeg in the current directory?'
+            message: `Would you like to install ffmpeg in the current directory?`,
+            directory: app.get('dirName')
         });
         return;
     }
-
     const ffmpeg = app.get('ffmpeg');
     if (ffmpeg && ffmpeg.running) {
         return renderVideo(res, ffmpeg.params);
     }
 
-    return renderIndex(res);
+    return renderIndex(res, null, values);
 });
 
 router.post('/', function (req, res) {
@@ -64,10 +56,14 @@ router.post('/', function (req, res) {
     const body = req.body;
     switch (body.action) {
         case 'Exit':
+            res.render('exit', {
+                title: 'GAME OVER',
+                message: 'Insert coin to continue'
+            });
             process.exit(0);
             break;
         case 'Stop':
-            return renderIndex(res);
+            return renderIndex(res, null, values);
         case 'Install':
             const ffbinaries = require('ffbinaries');
             ffbinaries.downloadFiles('ffmpeg', {quiet: true, destination: app.get('dirName')}, function (err, data) {
@@ -85,108 +81,116 @@ router.post('/', function (req, res) {
                 ffbinaries.clearCache();
                 const configure = require('../lib/configure');
                 configure(app);
-                return renderIndex(res);
+                return renderIndex(res, null, null);
             });
             break;
         case 'Start':
 
-            if (!body.inputUrl) {
-                renderInputError(res, 'Input url is required.');
-                return;
-            }
+            /* +++++++++ gather form input values ++++++++++ */
+
+            values = body;
+
+            //mandatory, will be passed to ffmpeg-respawn
+            const logLevel = values.logLevel;
+
+            //mandatory
+            const hwAccel = values.hwAccel;
+
+            //optional
+            const analyzeDuration = values.analyzeDuration;
+
+            //optional
+            const probeSize = values.probeSize;
+
+            //mandatory
+            const inputType = values.inputType;
+
+            //mandatory
+            const inputUrl = values.inputUrl;
+
+            //optional
+            const rtspTransport = values.rtspTransport;
+
+            //mandatory
+            const mp4HlsListSize = values.mp4HlsListSize;
+
+            //mandatory
+            const mp4AudioCodec = values.mp4AudioCodec;
+
+            //mandatory
+            const mp4VideoCodec = values.mp4VideoCodec;
+
+            //optional
+            const mp4Rate = values.mp4Rate;
+
+            //optional
+            const mp4Scale = values.mp4Scale;
+
+            //optional
+            const fragDuration = values.fragDuration;
+
+            //optional
+            const mp4Crf = values.mp4Crf;
+
+            //optional
+            const mp4Preset = values.mp4Preset;
+
+            //optional
+            const mp4Profile = values.mp4Profile;
+
+            //optional
+            const mp4Level = values.mp4Level;
+
+            //mandatory
+            const jpegCodec = values.jpegCodec;
+
+            //optional
+            const jpegRate = values.jpegRate;
+
+            //optional
+            const jpegScale = values.jpegScale;
+
+            //optional
+            const jpegQuality = values.jpegQuality;
+
+            /* +++++++++ process form input values ++++++++++ */
 
             //params to be passed to ffmpeg
             const params = [];
 
-            /* +++++++++ gather form input values ++++++++++ */
-
-            //mandatory, will be passed to ffmpeg-respawn
-            const logLevel = body.logLevel;
-
-            //mandatory
-            const hwAccel = body.hwAccel;
-
-            //optional
-            const analyzeDuration = body.analyzeDuration;
-
-            //optional
-            const probeSize = body.probeSize;
-
-            //mandatory
-            const inputType = body.inputType;
-
-            //mandatory
-            const rtspTransport = body.rtspTransport;
-
-            //mandatory
-            const mp4AudioCodec = body.mp4AudioCodec;
-
-            //mandatory
-            const mp4VideoCodec = body.mp4VideoCodec;
-
-            //mandatory
-            const rate = body.mp4Rate;
-
-            //mandatory
-            const scale = body.mp4Scale;
-
-            //mandatory
-            const mp4PixFmt = body.mp4PixFmt;
-
-            //mandatory
-            const fragDuration = body.fragDuration;
-
-            //mandatory
-            const crf = body.crf;
-
-            //mandatory
-            const preset = body.preset;
-
-            //mandatory
-            const mp4Profile = body.mp4Profile;
-
-            //mandatory
-            const mp4Level = body.mp4Level;
-
-            //mandatory
-            const jpegRate = body.jpegRate;
-
-            //mandatory
-            const jpegScale = body.jpegScale;
-
-            //mandatory
-            const jpegQuality = body.jpegQuality;
-
-            /* +++++++++ process form input values ++++++++++ */
-
             params.push(...['-hwaccel', hwAccel]);
 
-            if (analyzeDuration) {
+            if (analyzeDuration !== 'none') {
                 params.push(...['-analyzeduration', analyzeDuration]);
             }
 
-            if (probeSize) {
+            if (probeSize !== 'none') {
                 params.push(...['-probesize', probeSize]);
             }
 
             switch (inputType) {
-                /*case 'artificial':
+
+                case 'artificial':
                     params.push(...['-re', '-f', 'lavfi', '-i', 'testsrc=size=1280x720:rate=15']);
-                    break;*/
+                    break;
 
                 case 'rtsp':
-                    if (body.inputUrl.indexOf('rtsp://') === -1) {
-                        return renderIndex(res, 'Input url must begin with rtsp://');
+                    if (inputUrl.indexOf('rtsp://') === -1) {
+                        return renderIndex(res, 'Input url must begin with rtsp://', values);
                     }
-                    params.push(...['-rtsp_transport', rtspTransport]);
-                    params.push(...['-i', body.inputUrl]);
+                    if (rtspTransport !== 'none') {
+                        params.push(...['-rtsp_transport', rtspTransport]);
+                    }
+                    params.push(...['-i', inputUrl]);
                     break;
+
                 case 'mjpeg':
-                    if (body.inputUrl.indexOf('http://') === -1 && body.inputUrl.indexOf('https://') === -1) {
-                        return renderIndex(res, 'Mjpeg url must begin with http(s)://');
+                    if (inputUrl.indexOf('http://') === -1 && inputUrl.indexOf('https://') === -1) {
+                        return renderIndex(res, 'Input url must begin with http(s)://', values);
                     }
-                    params.push(...['-re', '-i', body.inputUrl]);
+                    params.push(...['-re', '-i', inputUrl]);
                     break;
+
                 default:
                     throw new Error('unsupported input type');
             }
@@ -201,28 +205,61 @@ router.post('/', function (req, res) {
 
             if (mp4VideoCodec !== 'copy') {
 
-                params.push(...['-vf', `fps=${rate},scale=trunc(iw*${scale}/2)*2:-2,format=${mp4PixFmt}`]);
-
-                params.push(...['-min_frag_duration', fragDuration, '-frag_duration', fragDuration]);
-
-                params.push(...['-crf', crf]);
-
-                params.push(...['-preset', preset]);
-
                 params.push(...['-tune', 'zerolatency']);
 
-                params.push(...['-profile:v', mp4Profile]);
+                if (mp4Rate !== 'none') {
+                    params.push(...['-r', mp4Rate]);
+                }
 
-                params.push(...['-level', mp4Level]);
+                if (mp4Scale !== 'none') {
+                    params.push(...['-vf', `scale=trunc(iw*${mp4Scale}/2)*2:-2`]);
+                }
+
+                if (fragDuration !== 'none') {
+                    params.push(...['-min_frag_duration', fragDuration, '-frag_duration', fragDuration]);
+                }
+
+                if (mp4Crf !== 'none') {
+                    params.push(...['-crf', mp4Crf]);
+                }
+
+                if (mp4Preset !== 'none') {
+                    params.push(...['-preset', mp4Preset]);
+                }
+
+                if (mp4Profile !== 'none') {
+                    params.push(...['-profile:v', mp4Profile]);
+                }
+
+                if (mp4Level !== 'none') {
+                    params.push(...['-level', mp4Level]);
+                }
 
             }
 
             params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', 'pipe:1']);
-            //params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof+omit_tfhd_offset', 'pipe:1']);
 
-            params.push(...['-an', '-c:v', 'mjpeg', '-f', 'image2pipe', '-q:v', jpegQuality, '-vf', `fps=${jpegRate},scale=trunc(iw*${jpegScale}/2)*2:-2,format=yuv420p`, 'pipe:4']);
+            params.push(...['-c', jpegCodec]);
 
-            mp4frag = new M4F({hlsBase: 'test', hlsListSize: 4})
+            if (jpegCodec !== 'copy') {
+
+                if (jpegQuality !== 'none') {
+                    params.push(...['-q', jpegQuality]);
+                }
+
+                if (jpegRate !== 'none') {
+                    params.push(...['-r', jpegRate]);
+                }
+
+                if (jpegScale !== 'none') {
+                    params.push(...['-vf', `scale=trunc(iw*${jpegScale}/2)*2:-2`]);//,format=yuv420p
+                }
+
+            }
+
+            params.push(...['-f', 'image2pipe', 'pipe:4']);
+
+            mp4frag = new M4F({hlsBase: 'test', hlsListSize: mp4HlsListSize})
                 .on('error', (err)=> {
                 console.error(err.message);
                 //console.log(ffmpeg.running);
@@ -258,7 +295,7 @@ router.post('/', function (req, res) {
                     .start();
                 app.set('ffmpeg', ffmpeg);
             } catch (error) {
-                return renderIndex(res, error.message);
+                return renderIndex(res, error.message, values);
             }
             renderVideo(res, ffmpeg.params);
             break;

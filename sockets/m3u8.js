4b44014e692967d80193fc04dcfb8f72a7446e6f
Kevin Godell <kevin.godell@gmail.com>
2018-03-07 13:51:57 -0600

    tweaked m3u8 socket

--- sockets/m3u8.js
+++ sockets/m3u8.js
@@ -14,9 +14,13 @@ module.exports = function(app, io) {
             if (mp4frag.m3u8) {
                 socket.emit('m3u8', mp4frag.m3u8);
             }
-            socket.on('request', function() {
-                if (mp4frag.m3u8) {
+            const emitM3u8 = () => {
                     socket.emit('m3u8', mp4frag.m3u8);
+            };
+            socket.on('m3u8Request', emitM3u8);
+            socket.once('disconnect', () => {
+                if (socket) {
+                    socket.removeListener('m3u8Request', emitM3u8);
                 }
             });
         });

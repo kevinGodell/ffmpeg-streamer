8e56e3d7496cd14f4c369e9224bbaa7c79a86353
Kevin Godell <kevin.godell@gmail.com>
2018-03-07 20:50:31 -0600

    add stderr socket

---
+++ sockets/stderr.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const {Writable} = require('stream');
+const namespace = '/stderr';
+
+module.exports = (app, io) => {
+
+    io
+        .of(namespace)
+
+        .on('connection', (socket) => {
+
+            const stderrLogs = app.get('stderrLogs');
+
+            if (!stderrLogs) {
+                socket.disconnect();
+                return;
+            }
+
+            const writable = new Writable({
+                write(chunk, encoding, callback) {
+                    socket.emit('stderr', chunk.toString());
+                    callback();
+                }
+            });
+
+            stderrLogs.pipe(writable);
+
+            socket.once('disconnect', () => {
+
+                if (stderrLogs && writable) {
+                    stderrLogs.unpipe(writable);
+                }
+
+            });
+
+        });
+
+};

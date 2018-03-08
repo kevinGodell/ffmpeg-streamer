f9cb514b7dcb301bfc014cec0447b4e78f43a2be
Kevin Godell <kevin.godell@gmail.com>
2018-03-08 15:02:04 -0600

    add new listener to catch first creation of m3u8 playlist

--- sockets/m3u8.js
+++ sockets/m3u8.js
@@ -2,26 +2,41 @@
 
 const namespace = '/m3u8';
 
-module.exports = function(app, io) {
+module.exports = (app, io) => {
     io
+
         .of(namespace)
-        .on('connection', (socket)=> {
+
+        .on('connection', (socket) => {
+
             const mp4frag = app.get('mp4frag');
+
             if (!mp4frag) {
                 socket.disconnect();
                 return;
             }
-            if (mp4frag.m3u8) {
-                socket.emit('m3u8', mp4frag.m3u8);
-            }
+
             const emitM3u8 = () => {
                 socket.emit('m3u8', mp4frag.m3u8);
             };
-            socket.on('m3u8Request', emitM3u8);
+
+            if (mp4frag.m3u8) {
+                socket.emit('m3u8', mp4frag.m3u8);
+            } else {
+                mp4frag.once('initialized', emitM3u8);
+            }
+
+            mp4frag.on('segment', emitM3u8);
+
             socket.once('disconnect', () => {
-                if (socket) {
-                    socket.removeListener('m3u8Request', emitM3u8);
+
+                if (mp4frag) {
+                    mp4frag.removeListener('initialized', emitM3u8);
+                    mp4frag.removeListener('segment', emitM3u8);
                 }
+
             });
+
         });
+
 };

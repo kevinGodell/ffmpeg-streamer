c50d3703200390e9f62ea4f259fec9ba8a6a4ff0
Kevin Godell <kevin.godell@gmail.com>
2018-03-08 15:03:31 -0600

    linting

--- sockets/progress.js
+++ sockets/progress.js
@@ -2,29 +2,45 @@
 
 const namespace = '/progress';
 
-module.exports = function(app, io) {
+module.exports = (app, io) => {
+
     io
+
         .of(namespace)
-        .on('connection', (socket)=> {
+
+        .on('connection', (socket) => {
+
             const ffmpeg = app.get('ffmpeg');
+
             if (!ffmpeg) {
                 socket.disconnect();
                 return;
             }
+
             if (ffmpeg.progress) {
                 socket.emit('progress', ffmpeg.progress);
             }
+
             const emitProgress = () => {
+
                 if (!ffmpeg.progress) {
                     return;
                 }
+
                 socket.emit('progress', ffmpeg.progress);
+
             };
+
             socket.on('progressRequest', emitProgress);
+
             socket.once('disconnect', () => {
+
                 if (socket) {
                     socket.removeListener('progressRequest', emitProgress);
                 }
+
             });
+
         });
+
 };

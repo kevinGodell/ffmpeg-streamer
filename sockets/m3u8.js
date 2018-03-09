c55eccd876b5a0c927ad8e5aa50305aae952ca88
Kevin Godell <kevin.godell@gmail.com>
2018-03-09 12:02:21 -0600

    changed function declaration

--- sockets/m3u8.js
+++ sockets/m3u8.js
@@ -10,16 +10,16 @@ module.exports = (app, io) => {
 
         .on('connection', (socket) => {
 
-            function emitM3u8() {
-                socket.emit('m3u8', mp4frag.m3u8);
-            }
-
             const mp4frag = app.get('mp4frag');
 
             if (!mp4frag) {
                 socket.disconnect();
                 return;
             }
+
+            const emitM3u8 = () => {
+                socket.emit('m3u8', mp4frag.m3u8);
+            };
 
             if (mp4frag.m3u8) {
                 socket.emit('m3u8', mp4frag.m3u8);

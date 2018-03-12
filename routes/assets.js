91f0e51d3839a2cc433a64112ece4717325bad6c
Kevin Godell <kevin.godell@gmail.com>
2018-03-12 01:02:08 -0500

    updated to be compatible with pkg

--- routes/assets.js
+++ routes/assets.js
@@ -7,6 +7,9 @@ const path = require('path');
 const flvMinJs = path.join(__dirname, '../node_modules/flv.js/dist/flv.min.js');
 const flvMinJsMap = path.join(__dirname, '../node_modules/flv.js/dist/flv.min.js.map');
 const hlsMinJs = path.join(__dirname, '../node_modules/hls.js/dist/hls.min.js');
+//const materialMinJs = path.join(__dirname, '../node_modules/material-design-lite/dist/material.min.js');
+//const materialDeepOrangeBlueMinCss = path.join(__dirname, '../node_modules/material-design-lite/dist/material.deep_orange-blue.min.css');
+//const materialMinJsMap = path.join(__dirname, '../node_modules/material-design-lite/dist/material.min.js.map');
 
 router.get('/', (req, res) => {
     res.send('assets router');
@@ -23,5 +26,17 @@ router.get('/flv.min.js.map', (req, res) => {
 router.get('/hls.min.js', (req, res) => {
     res.sendFile(hlsMinJs);
 });
+
+/*router.get('/material.min.js', (req, res) => {
+    res.sendFile(materialMinJs);
+});
+
+router.get('/material.min.js.map', (req, res) => {
+    res.sendFile(materialMinJsMap);
+});
+
+router.get('/material.deep_orange-blue.min.css', (req, res) => {
+    res.sendFile(materialDeepOrangeBlueMinCss);
+});*/
 
 module.exports = router;

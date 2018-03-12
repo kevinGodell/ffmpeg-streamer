afcc243f5732dc780ce7d7ef78db961b74c9b3f4
Kevin Godell <kevin.godell@gmail.com>
2018-03-11 20:21:52 -0500

    add param to ffmpeg

--- routes/index.js
+++ routes/index.js
@@ -269,7 +269,7 @@ router.post('/', (req, res) => {
 
             }
 
-            params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', 'pipe:1']);
+            params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof+omit_tfhd_offset', 'pipe:1']);
 
             params.push(...['-c', jpegCodec]);
 

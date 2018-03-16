50a8bb87666ff56d82e6b360158036c658f77508
Kevin Godell <kevin.godell@gmail.com>
2018-03-16 16:38:58 -0500

    add reset_timestamps to ffmpeg params

--- routes/index.js
+++ routes/index.js
@@ -269,7 +269,7 @@ router.post('/', (req, res) => {
 
             }
 
-            params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof+omit_tfhd_offset', 'pipe:1']);
+            params.push(...['-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof+omit_tfhd_offset', '-reset_timestamps', '1', 'pipe:1']);
 
             params.push(...['-c', jpegCodec]);
 

dc462c6f5ffc12358de10a1d7f2b3c20ac170b40
Kevin Godell <kevin.godell@gmail.com>
2018-03-16 18:38:34 -0500

    only emit m3u8 playlist on segment event for now

--- sockets/m3u8.js
+++ sockets/m3u8.js
@@ -22,17 +22,18 @@ module.exports = (app, io) => {
             };
 
             if (mp4frag.m3u8) {
-                socket.emit('m3u8', mp4frag.m3u8);
-            } else {
-                mp4frag.once('initialized', emitM3u8);
-            }
+                emitM3u8();
+                //socket.emit('m3u8', mp4frag.m3u8);
+            }// else {
+                //mp4frag.once('initialized', emitM3u8);
+            //}
 
             mp4frag.on('segment', emitM3u8);
 
             socket.once('disconnect', () => {
 
                 if (mp4frag) {
-                    mp4frag.removeListener('initialized', emitM3u8);
+                    //mp4frag.removeListener('initialized', emitM3u8);
                     mp4frag.removeListener('segment', emitM3u8);
                 }
 

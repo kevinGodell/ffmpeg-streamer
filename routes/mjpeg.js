5a6bb1063f4581799ed8af330f239da2cb33612d
Kevin Godell <kevin.godell@gmail.com>
2018-03-16 19:43:42 -0500

    simplified mjpeg file serving

--- routes/mjpeg.js
+++ routes/mjpeg.js
@@ -8,7 +8,7 @@ router.use('/', (req, res, next) => {
     const app = req.app;
     const pipe2jpeg = app.get('pipe2jpeg');
     if (!pipe2jpeg) {
-        res.sendStatus(503);
+        res.status(404).send('mjpeg not available');
         res.destroy();
         return;
     }
@@ -20,26 +20,13 @@ router.use('/', (req, res, next) => {
     next();
 });
 
+router.get('/', (req, res) => {
+    res.send('mjpeg router');
+});
+
 router.get('/test.mjpg', (req, res) => {
-    function onJpeg() {
-        res.set('Content-Type', 'multipart/x-mixed-replace;boundary=ffmpeg_streamer');
-        //res.set('Transfer-Encoding', '');
-        res.write('--ffmpeg_streamer\r\n');
-        res.write(`Content-Type: image/jpeg\r\nContent-Length: ${pipe2jpeg.jpeg.length}\r\n\r\n`);
-        res.write(pipe2jpeg.jpeg);
-        res.write('\r\n--ffmpeg_streamer\r\n');
-        pipe2jpeg.pipe(writable, {end: true});
-    }
-
-    function cleanup() {
-        if (pipe2jpeg) {
-            pipe2jpeg.removeListener('jpeg', onJpeg);
-            pipe2jpeg.unpipe(writable);
-        }
-    }
-
     const pipe2jpeg = res.locals.pipe2jpeg;
-
+    const jpeg = pipe2jpeg.jpeg;
     const writable = new Writable({
         write(chunk, encoding, callback) {
             res.write(`Content-Type: image/jpeg\r\nContent-Length: ${chunk.length}\r\n\r\n`);
@@ -48,17 +35,18 @@ router.get('/test.mjpg', (req, res) => {
             callback();
         }
     });
-
-    res.once('close', () => {
-        cleanup();
-    });
-
-    if (pipe2jpeg.jpeg) {
-        onJpeg();
-    } else {
-        pipe2jpeg.once('jpeg', onJpeg);
+    res.set('Content-Type', 'multipart/x-mixed-replace;boundary=ffmpeg_streamer');
+    res.write('--ffmpeg_streamer\r\n');
+    if (jpeg) {
+        writable.write(jpeg, {end: true});
     }
-
+    pipe2jpeg.pipe(writable);
+    res.once('close', () => {
+        if (pipe2jpeg && writable) {
+            pipe2jpeg.unpipe(writable);
+        }
+        res.destroy();
+    });
 });
 
 module.exports = router;
